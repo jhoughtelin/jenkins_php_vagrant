8c54ed61d88bd4d85886ee60dbd603e4b8e80e73
Josh Houghtelin <Josh@FindSomeHelp.com>
2014-05-01 02:29:43 -0600

    Added PHP extensions xsl, dob, libxml for phpDox support

--- puphpet/files/exec-once/install_php_tools.sh
+++ puphpet/files/exec-once/install_php_tools.sh
@@ -3,13 +3,14 @@
 #
 # Install PHPDox
 #
-wget http://phpdox.de/releases/phpdox.phar
-chmod +x phpdox.phar
-mv phpdox.phar /usr/local/bin/phpdox
+# wget http://phpdox.de/releases/phpdox.phar
+# chmod +x phpdox.phar
+# mv phpdox.phar /usr/local/bin/phpdox
 
-#
 # Install PHPUnit
-#
 wget https://phar.phpunit.de/phpunit.phar
 chmod +x phpunit.phar
 mv phpunit.phar /usr/local/bin/phpunit
+
+# Update PEAR (Although it should alraedy be current)
+pear upgrade PEAR
